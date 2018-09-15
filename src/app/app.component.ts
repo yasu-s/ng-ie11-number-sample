b3c0ac0aa381b842fbac8b0a15c94598c37036fa
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2018-09-15 11:53:17 +0900

    入力値を表示

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,19 @@ import { Component } from '@angular/core';
     <h2>IE11 Number Directive Sample</h2>
     <div>
       <ul>
-        <li>type=text:<input type="text"></li>
-        <li>type=number:<input type="number"></li>
-        <li>type=number+customNumber:<input type="number" customNumber></li>
+        <li>type=text:<input type="text" [(ngModel)]="text1"> - {{ text1 }}</li>
+        <li>type=number:<input type="number" [(ngModel)]="num1"> - {{ num1 }}</li>
+        <li>type=number+customNumber:<input type="number" customNumber [(ngModel)]="num2"> - {{ num2 }}</li>
       </ul>
     </div>
   `
 })
 export class AppComponent {
+
+  text1: string;
+
+  num1: number;
+
+  num2: number;
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CustomNumberDirective } from './custom-number.directive';
@@ -11,6 +12,7 @@ import { CustomNumberDirective } from './custom-number.directive';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
