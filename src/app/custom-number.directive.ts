7146c1b7334efcbb1e80caffed9f563887657f7a
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2018-09-26 21:05:30 +0900

    正規表現に変更

--- src/app/custom-number.directive.ts
+++ src/app/custom-number.directive.ts
@@ -27,7 +27,7 @@ export class CustomNumberDirective {
    */
   @HostListener('keypress', ['$event'])
   onKeyPress(event: KeyboardEvent) {
-    if (!'0123456789'.includes(event.key))
+    if (!/^\d*$/.test(event.key))
       event.preventDefault();
   }
 
